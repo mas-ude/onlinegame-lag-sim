74c870e6dcca5af8f9008faf6d28e2d316670139
Florian Metzger <fm@tru42.org>
2015-11-18 15:20:17 +0100

    minimal conversion, still wrong results

--- simulation/R/lagsim.R
+++ simulation/R/lagsim.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 ## parameters
 
 par.tickrate <- 60      # Hz=1/s
@@ -11,62 +13,59 @@ par.userinput.events <- 100 # number of simulated user inputs
 
 
 
+## init sim
+# y <- rep(0, par.userinput.events)     # e2e delays per user input
+tickrate.timedelta <- 1 / par.tickrate  * 1e3  # time interval between game ticks (ms) (ts)
+framerate.timedelta <- 1 / par.framerate * 1e3 # time interval between frame ticks (ms) (tr)
+
+## first ticks are uniformly distributed in intervals
+tickrate.timedelta.0 <- runif(1) * tickrate.timedelta   # (ms)
+framerate.timedelta.0 <- runif(1) * framerate.timedelta # (ms)
+
+## run sim
+A <- cumsum(rexp(par.userinput.events, rate = par.lambda) * 1e3)  # arrivals time points (ms)
+F <- cumsum(rexp(par.userinput.events, rate = par.lambda))        # finish time points (ms)
+D.up <- max(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
+D.down   <- max(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
+B <- max(0, rnorm(par.userinput.events, mean = par.server.mean, sd = par.server.sd))       # (ms) processing delay
+W <- rep(0, times = 3 * par.userinput.events)                     # waiting times (ms) due to discretization
+W <- matrix(W, ncol = 3)                                          # convert to matrix (alternatively use df)
 
 
-sim <- function(){
-
-  ## init sim
-  y <- rep(0, par.userinput.events)     # e2e delays per user input
-  tickrate.timedelta <- 1 / par.tickrate  * 1e3  # time interval between game ticks (ms) (ts)
-  framerate.timedelta <- 1 / par.framerate * 1e3 # time interval between frame ticks (ms) (tr)
+for(i in 1:par.userinput.events){
   
-  ## first ticks are uniformly distributed in intervals
-  tickrate.timedelta.0 <- runif(1) * tickrate.timedelta   # (ms)
-  framerate.timedelta.0 <- runif(1) * framerate.timedelta # (ms)
+  nextTickR <- ceiling((A[i] -   framerate.timedelta.0) / framerate.timedelta) * framerate.timedelta + framerate.timedelta.0 # waiting for input and processing in next tick
+  W[i,1] <- nextTickR - A[i]           # (ms)
+  arrivalAtServer <- nextTickR + D.up[i] # uplink
   
-  ## run sim
-  A <- cumsum(rexp(par.userinput.events, rate = par.lambda) * 1e3)  # arrivals time points (ms)
-  F <- cumsum(rexp(par.userinput.events, rate = par.lambda))        # finish time points (ms)
-  D.down <- max(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
-  D.up   <- max(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
-  B <- max(0, rnorm(par.userinput.events, mean = par.server.mean, sd = par.server.sd))       # (ms) processing delay
-  W <- rep(0, times = 3 * par.userinput.events)                     # waiting times (ms) due to discretization
-  W <- matrix(W, ncol = 3)                                          # convert to matrix (alternatively use df)
+  nextTickS <- ceil((arrivalAtServer - tickrate.timedelta.0) / tickrate.timedelta) * tickrate.timedelta + tickrate.timedelta.0 # processing input at next game tick
+  W[i,2] <- nextTickS - arrivalAtServer # (ms)
   
+  arrivalAtClient <- nextTickS + B[i] + D.down[i]
+  displayTickR <- (ceiling((arrivalAtClient - framerate.timedelta.0) / framerate.timedelta) + 1) * framerate.timedelta + framerate.timedelta.0 # render frame in next-next frame
+  W[i,3] <- displayTickR - arrivalAtClient
+  
+  F[i] <- displayTickR
 }
 
+e2e.lag <- F - A
+
+df <- data.frame(e2e.lag = e2e.lag, framerate = par.framerate)
+ggplot(df, aes(x=e2e.lag)) + stat_ecdf(lwd=1)
 
 
 
-# %% run sim
-# D=max(0,normrnd(par.net.mean,par.net.std,par.N,2)); % (ms) network delay up D(:,1) and down D(:,2)
-# B=max(0,normrnd(par.server.mean,par.server.std,par.N,1)); % (ms) processing delay
-# W=zeros(par.N,3); % waiting times (ms) due to discretization
-# for i=1:par.N
-# nextTickR = ceil((A(i)-tr0)/tr)*tr+tr0; % waiting for input and processing in next tick
-# W(i,1)=nextTickR - A(i); % (ms)
-# arrivalAtServer = nextTickR+D(i,1); % uplink
-# 
-# nextTickS = ceil((arrivalAtServer-ts0)/ts)*ts+ts0; % processing input at next game tick
-# W(i,2)=nextTickS - arrivalAtServer; % (ms)
-# 
-# arrivalAtClient = nextTickS + B(i) + D(i,2);
-# displayTickR = (ceil((arrivalAtClient-tr0)/tr)+1)*tr+tr0; % render frame in next-next frame
-# W(i,3)=displayTickR-arrivalAtClient;
-# 
-# F(i)=displayTickR;
-# end
-# 
-# res.y=F-A;
-# res.F=F;
-# res.A=A;
-# res.B=B;
-# res.D=D;
-# res.W=W;
-# res.ts0=ts0;
-# res.ts=ts;
-# res.tsi=ceil((max(F)-ts0)/ts); % number of ticks
-# 
-# res.tr0=tr0; % first tick
-# res.tr=tr; % tick interval
-# res.tri=ceil((max(F)-tr0)/tr); % number of ticks
+
+
+# res.F <- F
+# res.A <- A
+# res.B <- B
+# res.D <- D
+# res.W <- W
+# res.tickrate.timedelta.0 <- tickrate.timedelta.0
+# res.tickrate.timedelta <- tickrate.timedelta
+# res.tickrate.totalticks <- ceiling((max(F) - tickrate.timedelta.0) / tickrate.timedelta) # number of ticks
+
+# res.framerate.timedelta.0 <- framerate.timedelta.0 # first tick
+# res.framerate.timedelta <- framerate.timedelta # tick interval
+# res.framerate.totalticks <- ceiling((max(F) - framerate.timedelta.0) / framerate.timedelta) # number of ticks
