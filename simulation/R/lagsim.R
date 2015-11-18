670aa93aec3168b59c6f7f9c295efb28cfb649ff
Florian Metzger <fm@tru42.org>
2015-11-18 16:57:13 +0100

    fixed up direct conversion

--- simulation/R/lagsim.R
+++ simulation/R/lagsim.R
@@ -8,7 +8,7 @@ par.lambda <- 20         # user input / sec
 par.net.mean <- 20       # one-way network delay (ms)
 par.net.sd <- 5          # owd standard deviation (ms)
 par.server.mean <- 3     # mean game server delay (ms)
-par.server.std <- 0.1    # mean game server delay (ms)
+par.server.sd <- 0.1    # mean game server delay (ms)
 par.userinput.events <- 100 # number of simulated user inputs
 
 
@@ -25,9 +25,9 @@ framerate.timedelta.0 <- runif(1) * framerate.timedelta # (ms)
 ## run sim
 A <- cumsum(rexp(par.userinput.events, rate = par.lambda) * 1e3)  # arrivals time points (ms)
 F <- cumsum(rexp(par.userinput.events, rate = par.lambda))        # finish time points (ms)
-D.up <- max(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
-D.down   <- max(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
-B <- max(0, rnorm(par.userinput.events, mean = par.server.mean, sd = par.server.sd))       # (ms) processing delay
+net.delay.up   <- pmax(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
+net.delay.down <- pmax(0, rnorm(par.userinput.events, mean = par.net.mean, sd = par.net.sd))             # (ms) network delay up D(:,1) and down D(:,2)
+B <- pmax(0, rnorm(par.userinput.events, mean = par.server.mean, sd = par.server.sd))       # (ms) processing delay
 W <- rep(0, times = 3 * par.userinput.events)                     # waiting times (ms) due to discretization
 W <- matrix(W, ncol = 3)                                          # convert to matrix (alternatively use df)
 
@@ -36,12 +36,12 @@ for(i in 1:par.userinput.events){
   
   nextTickR <- ceiling((A[i] -   framerate.timedelta.0) / framerate.timedelta) * framerate.timedelta + framerate.timedelta.0 # waiting for input and processing in next tick
   W[i,1] <- nextTickR - A[i]           # (ms)
-  arrivalAtServer <- nextTickR + D.up[i] # uplink
+  arrivalAtServer <- nextTickR + net.delay.up[1] # uplink
   
-  nextTickS <- ceil((arrivalAtServer - tickrate.timedelta.0) / tickrate.timedelta) * tickrate.timedelta + tickrate.timedelta.0 # processing input at next game tick
+  nextTickS <- ceiling((arrivalAtServer - tickrate.timedelta.0) / tickrate.timedelta) * tickrate.timedelta + tickrate.timedelta.0 # processing input at next game tick
   W[i,2] <- nextTickS - arrivalAtServer # (ms)
   
-  arrivalAtClient <- nextTickS + B[i] + D.down[i]
+  arrivalAtClient <- nextTickS + B[i] + net.delay.down[i]
   displayTickR <- (ceiling((arrivalAtClient - framerate.timedelta.0) / framerate.timedelta) + 1) * framerate.timedelta + framerate.timedelta.0 # render frame in next-next frame
   W[i,3] <- displayTickR - arrivalAtClient
   
